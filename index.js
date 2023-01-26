eda225aedf84fa37e9d24a731be7c07dc4df7dba
miguel <miguelrusso2840@gmail.com>
2023-01-26 17:11:49 -0300

    subo nuevos archivos

---
+++ index.js
@@ -0,0 +1,13 @@
+var nombre ="alicia"
+var apellido="davis"
+
+console.log("registro del nombre y apellido de la persona")
+
+
+function datos de persona()
+ return nombre + apellido
+ 
+
+ getElementByid("demo").inner.HTML onclick(datos de la persona aqui)
+ console.log("en este click se ven los datos de la persona del cv")
+ 
