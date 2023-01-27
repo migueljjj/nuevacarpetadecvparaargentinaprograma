9ff3b2e0e205603e5b763288699f4cc97ff146fc
miguel <miguelrusso2840@gmail.com>
2023-01-27 02:34:38 -0300

    subo nuevos archivos

--- index.js
+++ index.js
@@ -2,17 +2,13 @@ var nombre ="alicia"
 var apellido="davis"
 
 console.log("registro del nombre y apellido de la persona")
-
-
-function datos de persona()
- return nombre + apellido
- 
-
- document.getElementByid("demo").innerHTML onclick(datos de la persona aqui)
- console.log("en este click se ven los datos de la persona del cv")
  
 
  function displayAge() {
     document.getElementById("demo").innerHTML
 = displayAge ()="20/06/89"
  }
+
+let miboton = document.getElementById('el boton')
+
+<button onclick="document.getElementById('demo').innerHTML=Date()">The time is?</button>
