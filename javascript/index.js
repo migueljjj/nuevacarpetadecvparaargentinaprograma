92758740e62f8730f3942ba1218b9f36ca88851d
miguel <miguelrusso2840@gmail.com>
2023-01-28 04:52:32 -0300

    subo nuevos archivos

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,13 @@
+/*envio de informacion ,en este caso ,un saludo*/
 alert("hola mundo")
 
+/*variables*/
 let nombre ="alicia"
 let apellido="davis"
 
 console.log("alicia")
 
-
+/*condicional if ,else*/
 let edad=18
 
 if edad>=18
@@ -16,3 +18,7 @@ else edad<=18
 
 return("mostrar edad es menor o igual a 18")
 
+/*array*/
+let ventas("mesas","sillas")
+
+console.log("ventas.length")
